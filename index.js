adc3569cab8c115483c3d74e0b6ac6c1c69d08cc
Jun <ljhg1124@gmail.com>
2020-02-07 11:49:11 +0900

    자바스크립트 공부
    기초

---
+++ index.js
@@ -0,0 +1,18 @@
+alert('Im Working . Im JS. Im Beautiful. Im worth it'); // 경고창 띄우기.
+console.log('Im Working . Im JS. Im Beautiful. Im worth it'); // 콘솔창으로 로그 띄우기.
+
+let a = 221;
+let b = a - 5;
+console.log(b);
+
+/* ==========================================================
+
+    Variable (변수)
+ 1. 변하는 수
+ 2. 첫째 - 변수 선언 
+    둘째 - 초기화
+    셋째 - 사용하면된다!
+
+    변수 let - 수학적 형태.
+ 
+=========================================================== */
